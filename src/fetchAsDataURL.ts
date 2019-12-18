d50deb0c0cbcb09a0378b917ce97a47ed66c1571
huangjianyong <huangjianyong@tuzhanai.com>
2019-12-18 17:53:32 +0800

    feat(fetchasdataurl.js): 请求文件流方法

---
+++ src/fetchAsDataURL.ts
@@ -0,0 +1,36 @@
+/*
+ * @Author: Sexy
+ * @Date: 2019-12-18 17:04:07
+ * @LastEditors  : Sexy
+ * @LastEditTime : 2019-12-18 17:31:46
+ * @Description: file content
+ */
+
+interface FetchResolve {
+  url: string;
+  result?: FileReader["result"];
+}
+
+function fetchAsDataURL(url: string) {
+  return new Promise<FetchResolve>((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+      const fr = new FileReader();
+      fr.onload = e => {
+        resolve({ url, result: e.target?.result });
+      };
+      fr.onerror = () => {
+        reject(Error(`read ${url} error`));
+      };
+      fr.readAsDataURL(xhr.response);
+    };
+    xhr.onerror = () => {
+      reject(Error(`fetch ${url} error`));
+    };
+    xhr.responseType = "blob";
+    xhr.open("GET", url);
+    xhr.send();
+  });
+}
+
+export default fetchAsDataURL;
