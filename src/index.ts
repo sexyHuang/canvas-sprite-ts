aadb73e5134267f4b83150f5eb8ac31c4d83868d
huangjianyong <huangjianyong@tuzhanai.com>
2019-12-18 14:15:07 +0800

    chore: 基础构建搭建

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: Sexy
+ * @Date: 2019-12-18 11:42:55
+ * @LastEditors: Sexy
+ * @LastEditTime: 2019-12-18 11:47:07
+ * @Description: file content
+ */
+const TEXT: string = "test it";
+console.log(TEXT);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/*
+ * @Author: Sexy
+ * @Date: 2019-09-26 14:14:25
+ * @LastEditors  : Sexy
+ * @LastEditTime : 2019-12-18 11:48:28
+ * @Description: file content
+ */
+const path = require('path');
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+  // Other options...
+};
