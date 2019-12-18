1915ae57335dde56f1a7ec4c9551c2c1bf0ecca8
huangjianyong <huangjianyong@tuzhanai.com>
2019-12-18 17:52:14 +0800

    refactor(bincache.js): ts重构缓存对象

---
+++ src/binCache.ts
@@ -0,0 +1,26 @@
+/*
+ * @Author: Sexy
+ * @Date: 2019-12-18 16:14:53
+ * @LastEditors: Sexy
+ * @LastEditTime: 2019-12-18 16:57:14
+ * @Description: file content
+ */
+let cacheMap: {
+  [key: string]: any;
+} = {};
+
+const binCache = {
+  get: (key: string) => cacheMap[key],
+  set: (key: string, value: any) => {
+    cacheMap[key] = value;
+    return { key, value };
+  },
+  delete: (key: string) => {
+    delete cacheMap[key];
+  },
+  clear: () => {
+    cacheMap = {};
+  }
+};
+
+export default binCache;
