9f33921698cc029036f7e9c2219428fe8758e38a
Sexy <353016313@qq.com>
2019-12-18 22:46:52 +0800

    feat(index.ts): 主要功能完成

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,16 @@
 import isCrossOrigin from './isCrossOrigin';
 import tryFromCache from './tryFromCache';
 
+interface CanvasSpriteConfig {
+  canvas: HTMLCanvasElement;
+  fps: number;
+  frames: number;
+  imageUrl: string;
+  loop: number;
+}
+
+const CANVAS_CS_ID_KEY = 'data-cs-id';
+
 const loadImage = (url: string) =>
   new Promise<HTMLImageElement>(async (resolve, reject) => {
     const src = await tryFromCache(url);
@@ -23,25 +33,40 @@ const loadImage = (url: string) =>
     img.src = src;
   });
 
-interface CanvasSpriteConfig {
-  canvas: HTMLCanvasElement;
-  fps: number;
-  frames: number;
-  imageUrl: string;
-}
-class CanvasSprite {
-  private canvas: HTMLCanvasElement;
-  private context: CanvasRenderingContext2D;
-  private frames: number;
-  private fps: number;
-  private spriteImgPromise: Promise<HTMLImageElement>;
-  constructor({ canvas, frames, imageUrl, fps = 24 }: CanvasSpriteConfig) {
+export default class CanvasSprite {
+  private canvas!: HTMLCanvasElement;
+  private context!: CanvasRenderingContext2D;
+  private frames!: number;
+  private fps!: number;
+  private spriteImgPromise!: Promise<HTMLImageElement>;
+  private activeFrameIndex: number = 0;
+  private reqId: number = 0;
+  private loop: number = -1;
+  private loopCount: number = 0;
+  private aniStop: boolean = true;
+  constructor(config: CanvasSpriteConfig) {
+    this.init(config);
+  }
+  private init({
+    canvas,
+    frames,
+    imageUrl,
+    fps = 24,
+    loop = -1
+  }: CanvasSpriteConfig) {
+    if (canvas.hasAttribute(CANVAS_CS_ID_KEY)) {
+      throw new Error('the canvas has sprite with it, call .destroy() first');
+    }
+    canvas.setAttribute(CANVAS_CS_ID_KEY, `cs-${Date.now()}`);
     this.canvas = canvas;
     this.context = canvas.getContext('2d') as CanvasRenderingContext2D;
     this.frames = frames;
     this.fps = fps;
+    this.loop = loop;
+
     const imgPromise = loadImage(imageUrl);
     this.spriteImgPromise = imgPromise;
+
     imgPromise.then(spriteImg => {
       const cWidth = spriteImg.width / frames;
       const cHeight = spriteImg.height;
@@ -49,15 +74,37 @@ class CanvasSprite {
       canvas.height = cHeight;
     });
   }
-  private async draw(frameIndex: number) {
-    const { context, frames } = this;
-    if (frameIndex >= frames) return;
-    const spriteImgRef = await this.spriteImgPromise;
+  private addActiveFrameIndex() {
+    const { loop, loopCount, frames, activeFrameIndex, reqId } = this;
+    if (activeFrameIndex < frames) this.activeFrameIndex += 1;
+    else if (loop === -1 || loop >= loopCount) {
+      cancelAnimationFrame(reqId);
+    } else {
+      this.loopCount += 1;
+      this.activeFrameIndex += 1;
+    }
+  }
+  private async spriteAnimate(then: number = Date.now()) {
+    if (this.aniStop) return;
+    const now = Date.now();
+    const delta = now - then;
+    const fpsInterval = 1000 / this.fps;
+    if (delta >= fpsInterval) {
+      then = now - (delta % fpsInterval);
+      this.addActiveFrameIndex();
+      await this.draw();
+    }
+    this.reqId = requestAnimationFrame(this.spriteAnimate.bind(this, then));
+  }
+  private async draw() {
+    const { context, frames, spriteImgPromise, activeFrameIndex } = this;
+    if (activeFrameIndex >= frames) return;
+    const spriteImgRef = await spriteImgPromise;
     const { width, height } = spriteImgRef;
     context.clearRect(0, 0, width, height);
     context.drawImage(
       spriteImgRef,
-      (frameIndex * width) / frames,
+      (activeFrameIndex * width) / frames,
       0,
       width / frames,
       height,
@@ -66,5 +113,31 @@ class CanvasSprite {
       width / frames,
       height
     );
+  }
+
+  play() {
+    this.aniStop = false;
+    this.spriteAnimate();
+  }
+  pause() {
+    this.aniStop = true;
+  }
+  stop() {
+    this.aniStop = true;
+    setTimeout(() => {
+      this.activeFrameIndex = 0;
+      this.draw();
+    }, 32);
+  }
+  destroy() {
+    const { width, height } = this.canvas;
+    this.aniStop = true;
+    this.context.clearRect(0, 0, width, height);
+    this.canvas.removeAttribute(CANVAS_CS_ID_KEY);
+    this.reqId = 0;
+  }
+  reset(config: CanvasSpriteConfig) {
+    this.destroy();
+    this.init(config);
   }
 }
